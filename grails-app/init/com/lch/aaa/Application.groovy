2878e0cbe85274393d8c3f28a63e1abe413d48e0
Lin Chinghui <linchinghui@gmail.com>
2016-06-09 15:06:41 +0800

    prepare building profile and libs

--- grails-app/init/com/lch/aaa/Application.groovy
+++ grails-app/init/com/lch/aaa/Application.groovy
@@ -2,9 +2,7 @@ package com.lch.aaa
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-
 import org.grails.core.io.DefaultResourceLocator
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
@@ -63,7 +61,25 @@ class Application extends GrailsAutoConfiguration implements EnvironmentAware {
 	]
 
 	static void main(String[] args) {
-		GrailsApp.run(Application, args)
+		// for v3.0:
+		// GrailsApp.run(Application, args)
+		// for v3.1:
+		def app = new GrailsApp(Application) {
+			@Override
+			protected void printBanner(org.springframework.core.env.Environment environment) {
+				println '''
+	▄████████  ▄████████    ▄████████    ▄████████
+	███    ███ ███    ███   ███    ███   ███    ███
+	███    █▀  ███    █▀    ███    █▀    ███    █▀
+	███        ███         ▄███▄▄▄       ███
+	███        ███        ▀▀███▀▀▀     ▀███████████
+	███    █▄  ███    █▄    ███    █▄           ███
+	███    ███ ███    ███   ███    ███    ▄█    ███
+	████████▀  ████████▀    ██████████  ▄████████▀
+'''
+			}
+		}
+		app.run(args)
 	}
 
     public static ConfigObject loadConfiguration(String configFile) { //, grails.util.Environment env = null) {
