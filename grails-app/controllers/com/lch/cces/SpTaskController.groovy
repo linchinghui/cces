dd2086a0988d8ae10852c7d0c94524e68f2b6775
Lin Chinghui <linchinghui@gmail.com>
2016-06-26 14:35:02 +0800

    bug fixed: CRUD of SP-task

--- grails-app/controllers/com/lch/cces/SpTaskController.groovy
+++ grails-app/controllers/com/lch/cces/SpTaskController.groovy
@@ -5,153 +5,157 @@ import com.lch.aaa.*
 
 class SpTaskController extends BaseController<SpTask> {
 
-    static namespace = Application.NAMESPACE_API
+static namespace = Application.NAMESPACE_API
 
-    SpTaskController() {
-        super(SpTask)
-    }
+	SpTaskController() {
+		super(SpTask)
+	}
 
-    private void resolveParameters(params) {
-        def compIds = params?.id?.split('\\|')
+	private void resolveParameters(params) {
+		def compIds = params?.id?.split('\\|')
 
-        if (compIds?.size() >= 1) {
-            if (compIds[0] != 'null') {
-                if (params?.projectId == null) {
-                    params['projectId'] = compIds[0]
-                }
-            } else {
-                params.remove('id') // to identify 'CREATE'
-            }
-
-            if (compIds?.size() >= 2 && compIds[1] != 'null' && params?.workedDate == null) {
-                params['workedDate'] = compIds[1]
-            }
-            if (compIds?.size() >= 3 && compIds[2] != 'null' && params?.employeeId == null) {
-                params['employeeId'] = compIds[2]
-            }
+		if (compIds?.size() >= 1) {
+			if (compIds[0] != 'null') {
+				if (params?.projectId == null) {
+					params['projectId'] = compIds[0]
+				}
+			} else {
+				params.remove('id') // to identify 'CREATE'
+			}
+			if (compIds?.size() >= 2 && compIds[1] != 'null' && params?.workedDate == null) {
+				params['workedDate'] = compIds[1]
+			}
+			if (compIds?.size() >= 3 && compIds[2] != 'null' && params?.employeeId == null) {
+				params['employeeId'] = compIds[2]
+			}
 		}
 
-        if (params?.projectId) {
-            params['projectId'] = URLDecoder.decode(params['projectId'])
-        }
-        if (! params?.parsedDate && params?.workedDate) {
+		if (params?.projectId) {
+			params['projectId'] = URLDecoder.decode(params['projectId'])
+		}
+		if (params?.workedDate && params?.workedDate != params?.parsedDate) {
 			params['workedDate'] = URLDecoder.decode(params.workedDate).take(10).replaceAll('-','\\/')
-            params['parsedDate'] = new SimpleDateFormat('yyyy/MM/dd').parse(params.workedDate)
-        }
-    }
+			params['parsedDate'] = new SimpleDateFormat('yyyy/MM/dd').parse(params.workedDate)
+		}
+	}
 
-    private List<SpTask> listAllSpTasks(Map params) {
-        if (params?.embed == 'true') { // list all
-            params.remove('max')
-            params.remove('offset')
-        }
-        resolveParameters(params)
+	private List<SpTask> listAllSpTasks(Map params) {
+		if (params?.embed == 'true') { // list all
+			params.remove('max')
+			params.remove('offset')
+		}
+		resolveParameters(params)
 
-        return SpTask.where {
-            if (params?.projectId)  { project.id  == params.projectId }
-            if (params?.workedDate) { workedDate  == params.parsedDate }
-            if (params?.employeeId) { employee.id == params.employeeId }
-        }.list(params)
-    }
+		return SpTask.where {
+			if (params?.projectId)  { project.id  == params.projectId }
+			if (params?.workedDate) { workedDate  == params.parsedDate }
+			if (params?.employeeId) { employee.id == params.employeeId }
+		}.list(params)
+	}
 
-    protected final List<SpTask> listAllResources(Map params) {
-        def spTasks = listAllSpTasks(params)
-        def assignTasks = null
+	protected final List<SpTask> listAllResources(Map params) {
+		def spTasks = listAllSpTasks(params)
+		def assignTasks = null
 
-        if (params?.embed == 'true') { // list all
-            //
-            // params.project and params.workedDate must not be null
-            //
-            def calendar = Calendar.instance
-            calendar.setTime(params.parsedDate)
-            def year = calendar.get(Calendar.YEAR)
-            def week = calendar.get(Calendar.WEEK_OF_YEAR)
-            def dx   = calendar.get(Calendar.DAY_OF_WEEK) - 1
+		if (params?.embed == 'true') { // list all
+			//
+			// params.project and params.workedDate must not be null
+			//
+			def calendar = Calendar.instance
+			calendar.setTime(params.parsedDate)
+			def year = calendar.get(Calendar.YEAR)
+			def week = calendar.get(Calendar.WEEK_OF_YEAR)
+			def dx   = calendar.get(Calendar.DAY_OF_WEEK) - 1
 
-            def cloneTask = createResource(params)
-            def theProject = cloneTask.project
-            cloneTask.project = null
+			def cloneTask = createResource(params)
+			def theProject = cloneTask.project
+			cloneTask.project = null
 
-            assignTasks = Assignment.where {
-                eq('project', theProject)
-                eq('year', year)
-                eq('week', week)
-                eq("d$dx", true)
+			assignTasks = Assignment.where {
+				notIn 'employee', SpTask.where {
+					project    == theProject
+					workedDate == params.parsedDate
+				}.employee
 
-                not {
-                    exists SpTask.where {
-                        project    == theProject
-                        workedDate == params.parsedDate
-                    }.id()
-                }
+				eq    'project',  theProject
+				eq    'year',     year
+				eq    'week',     week
+				eq    "d$dx",     true
 
-            }.collect {
-                def task = new SpTask(cloneTask.properties)
-                task.employee = it.employee
-                return task
-            }
-        }
+			}.collect {
+				def task = new SpTask(cloneTask.properties)
+				task.employee = it.employee
+				return task
+			}
+		}
 
-        if (assignTasks) {
-            spTasks += assignTasks
-        }
+		if (assignTasks) {
+			spTasks += assignTasks
+		}
 
-        return spTasks
-    }
+		return spTasks
+	}
 
-    protected final SpTask queryForResource(Serializable id) {
-        listAllSpTasks(params)[0]
-    }
+	protected final SpTask queryForResource(Serializable id) {
+		listAllSpTasks(params)[0]
+	}
 
-    private void prepareDefaultValue(props) {
-    	if (props.project) {
-	        if (! props.constructPlace) props.constructPlace = props.project.constructPlace
-	        if (! props.equipment     ) props.equipment      = props.project.description
-        //  if (! props.constructCode ) props.constructCode  = props.project.constructCode
-            if (! props.constructType ) props.constructType  = props.project.constructType // substitute of constructCode
-	    //  if (! props.note          ) props.note           = props.project.note
-    	}
-    }
+	private void prepareDefaultValue(props) {
+		if (props.project) {
+		if (! props.constructPlace) props.constructPlace = props.project.constructPlace
+		if (! props.equipment     ) props.equipment      = props.project.description
+		// if (! props.constructCode ) props.constructCode  = props.project.constructCode
+		if (! props.constructType ) props.constructType  = props.project.constructType // substitute of constructCode
+		// if (! props.note          ) props.note           = props.project.note
+		}
+	}
 
-    protected final SpTask createResource(Map params) {
-        resolveParameters(params)
-        def props = params
+	protected final SpTask createResource(Map params) {
+		resolveParameters(params)
+		def props = params
 
-        if (params?.projectId &&
-            params?.projectId != 'null') {
-            props.project = Project.get(params.projectId)
-            props.remove('projectId')
-        }
-        if (params.workedDate &&
-        	params?.workedDate != 'null') {
-            // props.workedDate = params.parsedDate
-            props.workedDate = params.workedDate
-    	}
-        if (params?.employeeId &&
-            params?.employeeId != 'null') {
-            props.employee = Worker.get(params.employeeId)
-            props.remove('employeeId')
-        }
+		if (params?.projectId &&
+			params?.projectId != 'null') {
+			props.project = Project.get(params.projectId)
+			props.remove('projectId')
+		}
+		if (params.workedDate &&
+			params?.workedDate != 'null') {
+			props.workedDate = params.parsedDate
+		}
+		if (params?.employeeId &&
+			params?.employeeId != 'null') {
+			props.employee = Worker.get(params.employeeId)
+			props.remove('employeeId')
+		}
 
-        prepareDefaultValue(props)
+		prepareDefaultValue(props)
 		return resource.newInstance(props)
-    }
+	}
 
-    protected final SpTask createResource() {
-        return createResource(params)
-    }
+	protected final SpTask createResource() {
+		return createResource(params)
+	}
 
-    def edit() {
-        resolveParameters(params)
+	def edit() {
+		resolveParameters(params)
 
-        if (params?.id) {
-            super.edit()
+		if (params?.id) {
+			super.edit()
 
-        } else {
-            // redirect action: 'create', params: params
-            def url = g.createLink action: 'create', params: params
-            redirect url: url
-        }
-    }
+		} else {
+			// redirect action: 'create', params: params
+			def url = g.createLink action: 'create', params: params
+			redirect url: url
+		}
+	}
 
+	def update() {
+		resolveParameters(params)
+
+		if (params?.parsedDate) {
+			params.workedDate = params.parsedDate
+		}
+		super.update()
+	}
 }
