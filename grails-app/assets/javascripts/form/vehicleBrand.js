cca6b3eb4546846e8e081ffcaf37a04b99271c98
Lin Chinghui <linchinghui@gmail.com>
2017-03-20 23:04:26 +0800

    refine UI/JS structure of Vehicle and milage

--- grails-app/assets/javascripts/form/vehicleBrand.js
+++ grails-app/assets/javascripts/form/vehicleBrand.js
@@ -1,72 +1,73 @@
-//* require ../grid
 //= require_self
 
-var vehicleBrandList;
+function vehicleBrand(params) {
+	var vehicleBrandList;
 
-function removeBrandDataRequested(result) {
-	alertMessage(result);
-	if (result && result.status <=400) {
+	function removeBrandDataRequested(result) {
+		alertMessage(result);
+		if (result && result.status <= 400) {
+			reloadDataTables(vehicleBrandList);
+		}
+	}
+
+	function modifyBrandDataRequested(result, editForm) {
 		reloadDataTables(vehicleBrandList);
 	}
-}
 
-function modifyBrandDataRequested(result, editForm) {
-	reloadDataTables(vehicleBrandList);
-}
+	function addBrandDataRequested(result, editForm) {
+		reloadDataTables(vehicleBrandList);
+	}
 
-function addBrandDataRequested(result, editForm) {
-	reloadDataTables(vehicleBrandList);
-}
+	function addBrandDataRequest(evt, dt, node, config) {
+		BootstrapDialog.show({
+			title: '新增...',
+			message: requestAction4BootstrapDialog({
+				url: server.ctxPath + '/vehicleBrand/create',
+				callback: addBrandDataRequested
+			})
+		});
+	}
 
-function addBrandDataRequest(evt, dt, node, config) {
-	BootstrapDialog.show({
-		title: '新增...',
-		message: requestAction4BootstrapDialog({
-			url: server.ctxPath + '/vehicleBrand/create',
-			callback: addBrandDataRequested
-		})
-	});
-}
-
-function createBrandDataTable() {
-	vehicleBrandList = $('#list-vehicleBrand').DataTable({
-			processing: true,
-			serverSide: true,
-			deferRender: true,
-			ajax: {
-				url: server.ctxPath + '/api/vehicleBrands.json'
-			},
-			initComplete: function(settings, data) {
-				initialized4DataTables(settings, data);
-			},
-			extButtons: {
-				copy: true
-			},
-			buttons: [{
-				text: '新增',
-				action: addBrandDataRequest
-			}],
-			columns: [ //0
-				renderAlterationCellWithId4DataTables({
-					edit: {
-						url: server.ctxPath + '/vehicleBrand/edit',
-						callback: modifyBrandDataRequested
-					},
-					delete: {
-						url: server.ctxPath + '/vehicleBrand/delete',
-						callback: removeBrandDataRequested
+	(function createBrandDataTable() {
+		vehicleBrandList = $('#list-vehicleBrand').DataTable({
+				processing: true,
+				serverSide: true,
+				deferRender: true,
+				ajax: {
+					url: server.ctxPath + '/api/vehicleBrands.json'
+				},
+				initComplete: function(settings, data) {
+					initialized4DataTables(settings, data);
+				},
+				extButtons: {
+					copy: true
+				},
+				buttons: [{
+					text: '新增',
+					action: addBrandDataRequest
+				}],
+				columns: [ //0
+					renderAlterationCellWithId4DataTables({
+						edit: {
+							url: server.ctxPath + '/vehicleBrand/edit',
+							callback: modifyBrandDataRequested
+						},
+						delete: {
+							url: server.ctxPath + '/vehicleBrand/delete',
+							callback: removeBrandDataRequested
+						}
+					}), { //1
+						data: 'name'
+					}, { //2
+						orderable: false,
+						data: 'description'
 					}
-				}), { //1
-					data: 'name'
-				}, { //2
-					orderable: false,
-					data: 'description'
-				}
-			],
-			order: [
-					[1, 'asc']
-				] // prev: 'aaSorting'
-		})
-		.buttons()
-		.disable();
+				],
+				order: [
+						[1, 'asc']
+					] // prev: 'aaSorting'
+			})
+			.buttons()
+			.disable();
+	})();
 }
