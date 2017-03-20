134f11c14bab24db766aeedb2d477179b6008f16
Lin Chinghui <linchinghui@gmail.com>
2017-03-20 22:49:23 +0800

    add new helper functions of JS

--- grails-app/assets/javascripts/form/dynamicEnumHelper.js
+++ grails-app/assets/javascripts/form/dynamicEnumHelper.js
@@ -3,41 +3,32 @@ var projectTypes = {};
 var constructTypes = {};
 
 function loadDynamicEnums() {
+	var url = server.ctxPath + '/api/dynamicEnums.json';
+
 	chainAjaxCall({
-		url: server.ctxPath + '/api/dynamicEnums.json',
+		url: url,
 		method: 'GET',
 		cache: true,
 		async: false,
-		headers: {
-			'X-CCES-ACTION': 'projectTypes'
-		}
+		headers: { 'X-CCES-ACTION': 'projectTypes' }
 
 	}).chain(function(promise) {
 		if (promise.rc == 1) {
-			projectTypes = {
-				'na': '(無法取得[工作型態]代碼)'
-			}
-
+			projectTypes = { 'na': '(無法取得[工作型態]代碼)' }
 		} else {
 			projectTypes = promise.data;
 		}
-
 		return chainAjaxCall({
-			url: server.ctxPath + '/api/dynamicEnums.json',
+			url: url,
 			method: 'GET',
 			cache: true,
 			async: false,
-			headers: {
-				'X-CCES-ACTION': 'constructTypes'
-			}
+			headers: { 'X-CCES-ACTION': 'constructTypes' }
 		});
 
 	}).done(function(promise) {
 		if (promise.rc == 1) {
-			constructTypes = {
-				'na': '(無法取得[施作方式]代碼)'
-			}
-
+			constructTypes = { 'na': '(無法取得[施作方式]代碼)' }
 		} else {
 			constructTypes = promise.data;
 		}
--- grails-app/assets/javascripts/util/string.js
+++ grails-app/assets/javascripts/util/string.js
@@ -1,6 +1,16 @@
 /*
  * string utilities
  */
-function pad(num, len = 10, ch = 0) {
+function pad(num, len, ch) {
+	if (typeof(len) === 'undefined') { len = 10; }
+	if (typeof(ch) === 'undefined') { ch = 0; }
 	return (String(ch).repeat(len) + String(num)).slice(String(num).length)
+}
+
+function hideHalf(str) {
+	if (typeof(str) === 'undefined') {
+		return '';
+	}
+	var mid = str ? str.length / 2 : 2;
+	return str.slice(0, mid) + '<span class="hidden-xs">' + str.slice(mid) + '</span>';
 }
